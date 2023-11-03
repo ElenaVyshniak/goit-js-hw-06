a031df229a3771506377f46f22b1225f15c03166
ElenaVyshniak <138595658+ElenaVyshniak@users.noreply.github.com>
2023-11-03 11:10:16 +0200

    task2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,31 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+// Знаходимо елемент ul#ingredients
+const ingredientsList = document.getElementById("ingredients");
+
+// Створюємо фрагмент документа для ефективного додавання всіх елементів
+const fragment = document.createDocumentFragment();
+
+// Для кожного інгредієнта у масиві ingredients
+ingredients.forEach((ingredient) => {
+  // Створюємо новий елемент <li>
+  const listItem = document.createElement("li");
+
+  // Додаємо назву інгредієнта як текстовий вміст елемента <li>
+  listItem.textContent = ingredient;
+
+  // Додаємо клас "item" до створеного елемента
+  listItem.classList.add("item");
+
+  // Додаємо створений елемент <li> до фрагмента
+  fragment.appendChild(listItem);
+});
+
+// Вставляємо всі створені елементи <li> однією операцією у список ul#ingredients
+ingredientsList.appendChild(fragment);
