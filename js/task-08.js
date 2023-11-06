17fea5936d0368e389a9f708759b10ecf67db04b
ElenaVyshniak <138595658+ElenaVyshniak@users.noreply.github.com>
2023-11-06 22:14:09 +0200

    task08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const email = loginForm.elements.email.value;
+  const password = loginForm.elements.password.value;
+
+  if (email.trim() === "" || password.trim() === "") {
+    alert("Всі поля повинні бути заповнені!");
+    return;
+  }
+  const formData = {
+    email,
+    password,
+  };
+
+  console.log(formData);
+
+  loginForm.reset();
+});
