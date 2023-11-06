448b035e616fe5da29f9cfa11d2e7185c03d8758
ElenaVyshniak <138595658+ElenaVyshniak@users.noreply.github.com>
2023-11-06 22:28:28 +0200

    task10

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,29 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const inputElement = document.querySelector("#controls input");
+const createButton = document.querySelector("[data-create]");
+const destroyButton = document.querySelector("[data-destroy]");
+const boxesContainer = document.querySelector("#boxes");
+createButton.addEventListener("click", () => {
+  createBoxes(inputElement.value);
+});
+destroyButton.addEventListener("click", () => {
+  destroyBoxes();
+});
+function createBoxes(amount) {
+  const boxSize = 30;
+  const boxes = [];
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    box.style.width = `${boxSize}px`;
+    box.style.height = `${boxSize}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxes.push(box);
+    boxSize += 10;
+  }
+  boxesContainer.append(...boxes);
+}
+function destroyBoxes() {
+  boxesContainer.innerHTML = "";
+}
